b8b3c8b7756dbed4f0d037c74f6e02343d08d4c1
mcobos09 <marthacobos08@gmail.com>
2023-06-22 19:02:30 -0300

    modifique código

--- mc.js
+++ mc.js
@@ -10,18 +10,43 @@ var cambioColor = {
 
 }
 
-
 var item = document.getElementById('verde');
 item.addEventListener('click', function() {
-  document.body.style.backgroundColor = 'rgb(0, 153, 0)';
+  document.body.style.backgroundColor = 'rgb(104, 247, 104)';
 });
 
 
 var item = document.getElementById('blanco');
 item.addEventListener('click', function() {
-  document.body.style.backgroundColor = 'rgb(255,255,255)';
+  document.body.style.backgroundColor = 'rgb(255,250,250)';
 });
 
+
+
+
+
+function createParagraph() {
+  let para = document.createElement('article');
+  para.textContent = 'ACTUALMENTE REALIZÓ UN CURSO DE DESARROLLO DE SITIOS WEB, CON EIDOS Y CÓDIGO PALANTE.';
+  document.body.appendChild(para);
+} 
+
+
+
+
+
+
+
+
+
+/*for(let i = 0; i < 5; i++) {
+  console.log( i );
+
+ 
+const buttons = document.querySelectorAll('button');
+for(let i = 0; i < buttons.length ; i++) {
+  buttons[i].addEventListener('click', );
+}
 
 /*function crearHTMLButton = document.querySelector('#verde');
 item.addEventListener('click', function() {
